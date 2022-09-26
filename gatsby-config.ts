7b39687378125cfbfa9b74847e74cb5e2cf571b9
Felix Hoffmann <felix.emmanuel.hoffmann@gmail.com>
2022-09-27 00:37:33 +0200

    :wrench: Added eslint and prettier

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,37 +1,49 @@
-import type { GatsbyConfig } from 'gatsby';
+import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
-    pathPrefix: '/website',
-    siteMetadata: {
-        title: `website`,
-        siteUrl: `https://www.yourdomain.tld`,
+  pathPrefix: "/website",
+  siteMetadata: {
+    title: `website`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+  // If you use VSCode you can also use the GraphQL plugin
+  // Learn more at: https://gatsby.dev/graphql-typegen
+  graphqlTypegen: true,
+  plugins: [
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "\u0016G-VN3HENNJCY",
+        // TODO: Ensure anonymous collection of analytics
+        anonymize: true,
+      },
     },
-    // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-    // If you use VSCode you can also use the GraphQL plugin
-    // Learn more at: https://gatsby.dev/graphql-typegen
-    graphqlTypegen: true,
-    plugins: [
-        {
-            resolve: 'gatsby-plugin-google-analytics',
-            options: {
-                trackingId: '\u0016G-VN3HENNJCY',
-                // TODO: Ensure anonymous collection of analytics
-                anonymize: true,
-            },
+    "gatsby-plugin-image",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+    {
+      resolve: "gatsby-plugin-eslint",
+      options: {
+        test: /\.js$|\.jsx$|\.ts$|\.tsx$/,
+        exclude: /(node_modules|.cache|public)/,
+        stages: ["develop"],
+        options: {
+          emitWarning: true,
+          failOnError: false,
         },
-        'gatsby-plugin-image',
-        'gatsby-plugin-sitemap',
-        'gatsby-plugin-sharp',
-        'gatsby-transformer-sharp',
-        {
-            resolve: 'gatsby-source-filesystem',
-            options: {
-                name: 'images',
-                path: './src/images/',
-            },
-            __key: 'images',
-        },
-    ],
+      },
+    },
+  ],
 };
 
 export default config;
