2b56515a2fc947816df686114306b27196b4f2d9
Felix Hoffmann <felix.emmanuel.hoffmann@gmail.com>
2022-09-26 23:58:52 +0200

    :rocket: Created gatsby project and added github pages

---
+++ gatsby-config.ts
@@ -0,0 +1,37 @@
+import type { GatsbyConfig } from 'gatsby';
+
+const config: GatsbyConfig = {
+    pathPrefix: '/website',
+    siteMetadata: {
+        title: `website`,
+        siteUrl: `https://www.yourdomain.tld`,
+    },
+    // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+    // If you use VSCode you can also use the GraphQL plugin
+    // Learn more at: https://gatsby.dev/graphql-typegen
+    graphqlTypegen: true,
+    plugins: [
+        {
+            resolve: 'gatsby-plugin-google-analytics',
+            options: {
+                trackingId: '\u0016G-VN3HENNJCY',
+                // TODO: Ensure anonymous collection of analytics
+                anonymize: true,
+            },
+        },
+        'gatsby-plugin-image',
+        'gatsby-plugin-sitemap',
+        'gatsby-plugin-sharp',
+        'gatsby-transformer-sharp',
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: 'images',
+                path: './src/images/',
+            },
+            __key: 'images',
+        },
+    ],
+};
+
+export default config;
